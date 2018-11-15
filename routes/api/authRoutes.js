9e854e47da74257a3d995174b0f7d4fee4ec4b05
paulvvo <paulvo@live.ca>
2018-11-14 22:01:20 -0500

    fixed user model to export properly, added body parser to server and included middleware, and added incomplete register route

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -2,10 +2,15 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../../models/User");
+
 
 router.get("/test", (req,res) => {
 	res.json({msg: "auth route working"})
 })
 
+router.post("/register", (req,res) =>{
+	res.json("test");
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const mongoose  = require("mongoose");
-const app = express();
-
-const db = require("./config/keys.js").mongoURI;
+const bodyParser = require("body-parser");
 
 //Routes
 const authRoutes = require("./routes/api/authRoutes");
 const postRoutes = require("./routes/api/postRoutes");
 const profileRoutes = require("./routes/api/profileRoutes");
 
-app.use("/api/auths", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/profiles", profileRoutes);
-
+const db = require("./config/keys.js").mongoURI;
 mongoose
 .connect(db)
 .then(() => console.log("Connected"))
 .catch(console.log);
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+//Routes
+app.use("/api/auths", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/profiles", profileRoutes);
 
 app.get("/", (req,res) => {
 	res.json("Hello Worl!");
