5a7c05d342b4cd7b19e3facd1c7cb78913f78f89
paulvvo <paulvo@live.ca>
2018-11-18 22:01:08 -0500

    added isempty function that checks whether a value is empty or not, checks strings and objects

--- validation/register.js
+++ validation/register.js
@@ -1,1 +1,9 @@
 const Validator = require("validator");
+const isEmpty  = require("./isEmpty");
+
+const validateRegisterInput = (data) => {
+	let errors={};
+
+}
+
+module.exports = validateRegisterInput;
