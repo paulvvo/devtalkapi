23c6b8d672c840c17c887b39f1f9919c846a9a25
paulvvo <paulvo@live.ca>
2018-11-19 18:52:41 -0500

    added more validations to register, will now check for matching passwords, password length, valid emails

--- validation/register.js
+++ validation/register.js
@@ -13,6 +13,26 @@ const validateRegisterInput = (input) => {
 	input.password = isEmpty(input.password) ? "" : input.password;
 	input.password2 = isEmpty(input.password2) ? "" :input.password2;
 
+
+	if(!Validator.isLength(input.name, {min:2, max:30})){
+		errors.name = "Name must be between 2 and 30 characters";
+	}
+	if(Validator.isEmpty(input.name)){
+		errors.name = "Name is required";
+	}
+	if(!Validator.isEmail(input.email)){
+		errors.email = "Must Enter Valid Email";
+	}
+	if(!Validator.isLength(input.password, {min:6, max:30})){
+		errors.password = "Password must be 6 to 30 characters";
+	}
+	if(! Validator.equals(input.password, input.password2)){
+		errors.password2 = "Passwords Must Match";
+	}
+
+
+
+
 	if(Validator.isEmpty(input.name)){
 		errors.name = "Name is required";
 	}
@@ -27,10 +47,6 @@ const validateRegisterInput = (input) => {
 	}
 
 
-
-	if(!Validator.isLength(input.name, {min:2, max:30})){
-		errors.name = "Name must be between 2 and 30 characters";
-	}
 
 
 
