57dd668a1d7f36aaf4216b7ba224c48b651e2c78
paulvvo <paulvo@live.ca>
2018-11-20 23:50:37 -0500

    added profile route, protected by token, will give you current user's profile if you are logged in

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -9,10 +9,25 @@ const Profile = require("../../models/Profile");
 const User = require("../../models/Profile");
 
 
-
 router.get("/test", (req,res) => {
 	res.json({msg: "profile route working"})
 })
 
+// @route 	GET api/profile
+// @desc 		gets current user's profile
+// @access	private
 
+router.get("/", passport.authenticate("jwt", {session:false}), (req,res)=>{
+	const errors = {};
+
+	Profile.findOne({user:req.user.id})
+	.then(foundProfile =>{
+		if(!foundProfile){
+			errors.profile="No Profile was found for this User";
+			res.status(404).json(errors);
+		}else res.json(foundProfile);
+
+	})
+	.catch(err => res.status(404).json(err));
+})
 module.exports = router;
