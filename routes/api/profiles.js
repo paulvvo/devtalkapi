d3af841fb343dc2a3c710a75229f578be1d3d6a8
paulvvo <paulvo@live.ca>
2018-11-21 01:46:23 -0500

    created post route for creating new profiles, started creating new profile with parameters from the body not complete yet

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -30,4 +30,20 @@ router.get("/", passport.authenticate("jwt", {session:false}), (req,res)=>{
 	})
 	.catch(err => res.status(404).json(err));
 })
+
+// @route 	POST api/profile
+// @desc 		create new user profile
+// @access	private
+
+router.post("/", passport.authenticate("jwt", {session:false}), (req,res)=>{
+	const newProfile = {};
+	newProfile.user = req.user.id;
+	if(req.body.handle) newProfile.handle = req.body.handle;
+	if(req.body.company) newProfile.company = req.body.company;
+	if(req.body.website) newProfile.website = req.body.website;
+	if(req.body.location) newProfile.location = req.body.location;
+	if(req.body.bio) newProfile.bio = req.body.bio;
+	if(req.body.status) newProfile.status = req.body.status;
+	if(req.body.githubusername) newProfile.githubusername = req.body.githubusername;
+})
 module.exports = router;
