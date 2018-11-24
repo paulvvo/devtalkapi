ee56ec64c160e0e4d39456e822b060c30ebf77f2
paulvvo <paulvo@live.ca>
2018-11-23 23:22:59 -0500

    populate user property from profile so that it displays the name and avatar

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -21,6 +21,7 @@ router.get("/", passport.authenticate("jwt", {session:false}), (req,res)=>{
 	const errors = {};
 
 	Profile.findOne({user:req.user.id})
+	.populate('user', ['name', 'avatar'])
 	.then(foundProfile =>{
 		if(!foundProfile){
 			errors.profile="No Profile was found for this User";
