a4143a84033f1a714aeff8adfcc70165a6bf4813
paulvvo <paulvo@live.ca>
2018-11-23 21:37:19 -0500

    adding validation to profile, checking if status, skills and handle are empty or not and check to make sure handle is the appropriate length

--- validation/profile.js
+++ validation/profile.js
@@ -5,6 +5,23 @@ const isEmpty = require("./isEmpty");
 const validateProfileInput = (input) => {
 	const errors = {};
 
+	input.handle = isEmpty(input.handle)? "" : input.handle;
+	input.status = isEmpty(input.status)? "" : input.status;
+	input.skills = isEmpty(input.skills)? "" : input.skills;
+
+	if(!Validator.isLength(input.handle, {min:2, max:40})){
+		errors.handle = "Handle length must be between 2 and 40";
+	}
+	if(Validator.isEmpty(input.handle)){
+		errors.handle = "Handle Field Required";
+	}
+	if(Validator.isEmpty(input.status)){
+		errors.status = "Status Field Required";
+	}
+	if(Validator.isEmpty(input.skills)){
+		errors.skills = "Skills Field  Required";
+	}
+
 	return {
 		errors:errors,
 		isValid:isEmpty(errors)
