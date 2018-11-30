abb727f5710144053a1d73ca3dec9e81fc07b974
paulvvo <paulvo@live.ca>
2018-11-29 22:59:43 -0500

    added POST post route to create new posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,25 @@
-//post routes
+//routes for dealing with posts that users can use
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 
+const Post = require("../../models/Post");
 
-router.get("/test", (req,res) => {
-	res.json({msg: "post route working"})
+// @route		POST api/posts
+// @desc		create a new post
+// @access 	private
+router.post("/", passport.authenticate("jwt", {session:false}), (req,res) => {
+	const newPost = new Post({
+		user:req.user.id,
+		name:req.body.name,
+		text:req.body.text,
+		avatar:req.body.avatar,
+	})
+	newPost.save()
+	.then(createdPost => res.json(createdPost))
+	.catch(err => res.status(400).json(err));
+
+
+	// Post.create({})
 })
-
-
 module.exports=router;
